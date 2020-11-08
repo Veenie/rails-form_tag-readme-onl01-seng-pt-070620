f292776f31b2a530193c71ab295d7630f17599b2
Raveen K Sharma <raveen08@gmail.com>
2020-11-08 01:59:03 +0000

    Automatically backed up by Learn

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module BlogFlash
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+<<<<<<< HEAD
     
+=======
+    config.web_console.whitelisted_ips = 165.227.60.187
+>>>>>>> 652caa7efeca07382b8ffe18816a1896cfcdd15b
   end
 end
